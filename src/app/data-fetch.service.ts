67fbd284262da9f618951ba087ca5173bea84607
SuvamDatta <suvam.datta@ibm.com>
2024-09-14 12:19:16 +0530

    url

--- src/app/data-fetch.service.ts
+++ src/app/data-fetch.service.ts
@@ -6,8 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataFetchService {
-  private apiUrl = 'https://model-api-final-hackathon2024-trendtrackers-ai.rhh24cluster-dal10-b3c-32-b7fa127339e34d1cc8d1b3e2be243d89-0000.us-south.containers.appdomain.cloud/get_forecasted_data';
-  private PrevYrapiUrl = 'https://model-api-final-hackathon2024-trendtrackers-ai.rhh24cluster-dal10-b3c-32-b7fa127339e34d1cc8d1b3e2be243d89-0000.us-south.containers.appdomain.cloud/get_combined_data';
+  baseurl:string = 'https://forecaster-model-hackathon2024-trendtrackers-ai.rhh24cluster-dal10-b3c-32-b7fa127339e34d1cc8d1b3e2be243d89-0000.us-south.containers.appdomain.cloud/'
+  apiUrl:string = this.baseurl + 'get_forecasted_data';
+  PrevYrapiUrl:string = this.baseurl + 'get_combined_data';
 
   constructor(private http: HttpClient) {}
 
